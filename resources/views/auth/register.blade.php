fd04f347cd3467d50e779178416b02f6457f3f66
Bipasha88 <dolasaha88@gmail.com>
2021-09-13 06:47:13 -0700

    error message added in register view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,6 +12,18 @@
 
 <div class="container">
     <h2 style="text-align: center">Register</h2>
+    @if(count($errors) > 0 )
+        <div class="alert alert-danger  text-danger" role="alert">
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <ul class="p-0 m-0" style="list-style: none;">
+                @foreach($errors->all() as $error)
+                    <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form class="form-horizontal" action="{{ Route('registerpost') }}" method="post">
             @csrf
 
